dc50ceec809b2d3e1e9f3aef2ea8a04cc4983e65
taokaa <95309207+taokaa@users.noreply.github.com>
2021-12-07 12:07:39 +0900

    crrect TDOD.js. Add currentTasks and FinishedTasks
    TODO.jsはkeyのエラーを修正すれば完成。

--- src/script/components/TODO.js
+++ src/script/components/TODO.js
@@ -1,40 +1,56 @@
 import React from 'react';
 
-const list = [];
-let ID = 0;
+let list = [];
+let currentList = [];
+let finishedList = [];
+let Index = 0;
 
 class TODO extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {id: '', value: ''};
+        this.state = {index: '', value: ''};
     }
 
-    handleChange(Value) {
-        this.setState({id: ID, value: Value});
-        console.log('handleChange'+ID+Value);
+    handleChange(value) {
+        this.setState({index: Index, value: value});
+        console.log('handleChange'+Index+value);
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.setState({id: '', value: ''});
-        ID++;
+        this.setState({ndex: '', value: ''});
+        Index++;
     }
 
     listItems(object) {
         list.push(object);
-        console.log(list);
+        currentList.push(object);
+    }
+
+    FinishedButton(index, value) {
+        this.setState({index: Index, value: this.state.value});
+        finishedList.push({index: index, value: value})
+        currentList = currentList.filter((item) => {
+            return item.index !== index;
+        });
+        console.log('list: '+list);
+        console.log('currentList: ' + currentList);
+        console.log('finishedList: ' + finishedList);
     }
 
     render() {
         return(
             <div>
                 <h1>
-                    This is ONLY ADD TODO list
+                    This is TODO list
                 </h1>
                 <form onSubmit={(e) => {
-                    this.listItems(this.state)
-                    console.log(this.state)
-                    this.handleSubmit(e)
+                    e.preventDefault()
+                    if (this.state.value) {
+                        this.listItems(this.state)
+                        console.log(this.state)
+                        this.handleSubmit(e)    
+                    }
                 }}>
                     <input
                         value={this.state.value} 
@@ -43,13 +59,32 @@ class TODO extends React.Component {
                         }}
                     >
                     </input>
-                    <button type>add task</button>
-                    <ul>
-                        {list.map((listItem) => {
-                            return <li key={listItem.id}>{listItem.value}</li>;
-                        })}
-                    </ul>
+                    <button type='submit'>add task</button>
                 </form>
+                <h2>Current Tasks</h2>
+                <ul>
+                    {currentList.map((listItem) => {
+                        return (
+                            <React.Fragment>
+                                <li key={listItem.index}>{listItem.value}</li>
+                                <button onClick={() => {
+                                    console.log('button: ' + listItem.index)
+                                    this.FinishedButton(listItem.index, listItem.value)
+                                }
+                                }>
+                                    finish!!
+                                </button>
+                            </React.Fragment>
+                        )
+                    })} 
+                </ul>
+                <h2>Finised Tasks</h2>
+                <ul>
+                    {finishedList.map((listItem) => {
+                        return (<li key={listItem.index}>{listItem.value}</li>)
+                    })}  
+                </ul>
+                
             </div>
         );
     }
